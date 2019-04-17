8a154a7224add096996a8ef4d8d6fb7f99f688cd
cgeegarcia <48723423+cgeegarcia@users.noreply.github.com>
2019-04-17 19:37:06 -0400

    Initial commit

---
+++ src/com/company/Customer.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Customer {
+
+    private int PhoneNumber;
+    private String CustomerFirstName;
+    private String CustomerLastName;
+    private String Address;
+    private String Email;
+    private int PaymentMethod;
+    private String Car;
+
+    public static void walkForward(){}
+    public static void turnLeft(){}
+    public static void turnRight(){}
+    public static void payMent(){}
+    public static void useHands(){}
+
+    public String getCustomerFirstName() {return CustomerFirstName;}
+    public String getCustomerLastName() {return CustomerLastName;}
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Customer CustomerFirstName = new Customer();
+        Customer PhoneNumber = new Customer();
+        Customer CustomerLastName = new Customer();
+        Customer Email = new Customer();
+        Customer Address = new Customer();
+        Customer Car = new Customer();
+        Customer PaymentMethod = new Customer();
+	
+    }
+}
